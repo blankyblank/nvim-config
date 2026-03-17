88ecad23d1b8ef8656d9ec1515962ed478594d04
blank <jwthaparc@gmail.com>
2026-03-17 01:07:26 -0500

    moved to a plugin for onedark, it has a few extra features

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-vim.cmd([[
-colorscheme onedark
-highlight Normal guibg=none
-highlight NonText guibg=none
-highlight Normal ctermbg=none
-highlight NonText ctermbg=none
-]])
-local highlight = { "CursorLine", "Whitespace" }
+-- vim.cmd([[
+-- colorscheme onedark
+-- highlight Normal guibg=none
+-- highlight NonText guibg=none
+-- highlight Normal ctermbg=none
+-- highlight NonText ctermbg=none
+-- ]])
+-- local highlight = { "CursorLine", "Whitespace" }
 
 autocmd = vim.api.nvim_create_autocmd
 vim.have_nerd_font = true
---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,51 @@
+vim.pack.add({
+  Gh("olimorris/onedarkpro.nvim"),
+})
+
+local color = require('onedarkpro.helpers')
+require("onedarkpro").setup({
+
+  colors = {
+    float_bg = "#282c34",
+    inlay_hint = "#7f848e",
+    params = "require('onedarkpro.helpers').lighten('red', 7, 'onedark')",
+    consts = "#e5c07b",
+    ops = "#E6E6E6",
+    funcs = "#abb2bf",
+    var_ro = "require('onedarkpro.helpers').lighten('yellow', 7, 'onedark')",
+    virtual_text_error = "require('onedarkpro.helpers').darken('red', 30, 'onedark')",
+    virtual_text_warning = "require('onedarkpro.helpers').darken('yellow', 30, 'onedark')",
+    virtual_text_information = "require('onedarkpro.helpers').darken('blue', 20, 'onedark')",
+    virtual_text_hint = "require('onedarkpro.helpers').darken('cyan', 10, 'onedark')",
+  },
+
+  highlights = {
+    Comment = { fg = "#7f848e", italic = true },
+    Identifier = { fg = "${funcs}"  },
+    Operator = { fg = "${ops}", bold = true },
+    Function = { fg = "${funcs}", bold = true, italic = true },
+    ["@function.builtin.c"] = { fg = "${funcs}", italic = true },
+    ["@lsp.typemod.function.defaultLibrary.c"] = { fg = "${funcs}", italic = true },
+    ["@character.printf"] = { fg = "#d19a66" },
+    ["@constant.c"] = { fg = "${consts}" },
+    ["@constant.builtin.c"] = { fg = "${consts}" },
+    ["@variable.c"] = { fg = "#e06c75" },
+    ["@lsp.type.variable.c"] = { fg = "#e06c75" },
+    ["@lsp.typemod.variable.readonly.c"] = { fg = "${var_ro}", bold = true },
+    ["@variable.parameter.c"] = { fg = "${params}", bold = true, italic = true },
+    Statement = { fg = "${ops}" },
+    ["@Keyword.modifier.c"] = { fg = "${ops}" },
+    ["@type.builtin.c"] = { fg = "${funcs}" },
+    ["@type.qualifier.c"] = { fg = "${funcs}" },
+    Keyword = { fg = "${ops}" },
+    ["@lsp.type.macro.c"] = { fg = "#d19a66" },
+    ["@Keyword.return.c"] = { fg = "#61AFEF" },
+    ["@Keyword.import.c"] = { fg = "#61AFEF" },
+    ["@label.c"] = { fg = "#61AFEF" },
+  },
+
+  options = { cursorline = true, terminal_colors = false },
+  filetypes = { all = false, },
+  plugins = { treesitter = false, },
+})
+vim.cmd("colorscheme onedark")
