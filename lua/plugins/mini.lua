fb696f8684cc98d1881681b71fa0fb719346a9c0
blankyblank <jwthaparc@gmail.com>
2026-04-03 02:13:49 -0500

    moved to only installing the parts of mini I use

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,11 +1,23 @@
-vim.pack.add({ Gh('nvim-mini/mini.nvim') })
+vim.pack.add({
+  Gh('nvim-mini/mini.align'),
+  Gh('nvim-mini/mini.bracketed'),
+  Gh('nvim-mini/mini.diff'),
+  Gh('nvim-mini/mini-git'),
+  Gh('nvim-mini/mini.icons'),
+  Gh('nvim-mini/mini.jump'),
+  Gh('nvim-mini/mini.move'),
+  Gh('nvim-mini/mini.pairs'),
+  Gh('nvim-mini/mini.surround'),
+  Gh('nvim-mini/mini.clue'),
+})
 
+require('mini.align').setup({})
 require('mini.bracketed').setup({})
 require('mini.diff').setup()
 require('mini.git').setup()
 require('mini.icons').setup()
 MiniIcons.mock_nvim_web_devicons()
-require('mini.jump').setup({ options = { silent = true } })
+require('mini.jump').setup({ silent = true  })
 require('mini.move').setup()
 require('mini.pairs').setup()
 require('mini.surround').setup({ respect_selection_type = true, silent = true })
