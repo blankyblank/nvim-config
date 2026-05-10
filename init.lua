ed342d7d047fb601f791c877e3645f49e0c42493
blankyblank <jwthaparc@gmail.com>
2026-05-10 04:25:12 -0500

    disable plugins for performance, added ft fix at end

--- init.lua
+++ init.lua
@@ -13,6 +13,42 @@ vim.have_nerd_font = true
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
+vim.g.autoformat = true
+
+-- disable loading plugins for performance
+vim.g.loaded_2html_plugin = true
+vim.g.loaded_bugreport = 1
+vim.g.loaded_compiler = 1
+vim.g.loaded_ftplugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_logipat = 1
+-- vim.g.loaded_matchit = 1
+-- vim.g.loaded_matchparen = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_optwin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_rplugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = true
+vim.g.loaded_synmenu = 1
+vim.g.loaded_syntax = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tohtml = 1
+vim.g.loaded_tutor = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+
 
 -- require("extra")
 require("config.hooks")
@@ -20,3 +56,15 @@ require("plugins")
 require("config")
 vim.cmd([[set runtimepath+=/usr/local/share/vim/vimfiles]])
 vim.cmd([[let &packpath = &runtimepath]])
+
+vim.filetype.add({
+  extension = {
+    h = function(path, bufnr)
+      -- Only set to cpp if it includes a C++ header (e.g., <vector>, <string>)
+      if vim.fn.search([[^#include\s*<[^>.]+>]], "nw", bufnr) > 0 then
+        return "cpp"
+      end
+      return "c"
+    end,
+  },
+})
