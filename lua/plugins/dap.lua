a1eaa00401f8e6f2fc5f9359d7320413ccd38030
blankyblank <jwthaparc@gmail.com>
2026-05-10 04:43:22 -0500

    still trying to find a dap config that doesn't suck

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,10 +2,12 @@ vim.pack.add({
   -- "https://github.com/mfussenegger/nvim-dap",
   Gh("mfussenegger/nvim-dap"),
   -- "https://github.com/rcarriga/nvim-dap-ui"
-  Gh("rcarriga/nvim-dap-ui"),
+  -- Gh("rcarriga/nvim-dap-ui"),
   Gh("nvim-neotest/nvim-nio"),
   -- Gh("folke/lazydev.nvim"), --lazydev might only be reccommended for lua
-  -- Gh("igorlfs/nvim-dap-view"),
+  Gh("igorlfs/nvim-dap-view"),
+  Gh("theHamsta/nvim-dap-virtual-text"),
+  Gh("rcarriga/cmp-dap"),
 })
 
 -- TODO:
@@ -14,70 +16,6 @@ vim.pack.add({
 --      or dap-view as good or better than termdebug
 
 local dap = require("dap")
-local dapui = require("dapui")
-dap.adapters.lldb = {
-  type = "executable",
-  command = "codelldb",
-  name = "lldb",
-}
-
-local lldb = {
-  {
-    name = "Launch",
-    type = "lldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-    args = {}, -- provide arguments if needed
-    runInTerminal = false,
-  },
-  {
-    name = "Select and attach to process",
-    type = "lldb",
-    request = "attach",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    pid = function()
-      local name = vim.fn.input("Executable name (filter): ")
-      return require("dap.utils").pick_process({ filter = name })
-    end,
-    cwd = "${workspaceFolder}",
-  },
-}
-
-dap.configurations.c = {
-  {
-    name = "Launch",
-    type = "lldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-    args = {}, -- provide arguments if needed
-    runInTerminal = false,
-  },
-  {
-    name = "Select and attach to process",
-    type = "lldb",
-    request = "attach",
-    program = function()
-      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-    end,
-    pid = function()
-      local name = vim.fn.input("Executable name (filter): ")
-      return require("dap.utils").pick_process({ filter = name })
-    end,
-    cwd = "${workspaceFolder}",
-  },
-}
-
---[[ local dap = require("dap")
 dap.adapters.gdb = {
   type = "executable",
   command = "gdb",
@@ -118,24 +56,60 @@ dap.configurations.c = {
     end,
     cwd = "${workspaceFolder}",
   },
-} ]]
-require("dapui").setup()
+}
 
-dap.listeners.before.attach.dapui_config = function()
-  dapui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dapui.open()
+require("dap-view").setup()
+require("nvim-dap-virtual-text").setup()
+
+vim.keymap.set('n', "<leader>tv", "<cmd>DapVirtualTextToggle<CR>", { desc = "Dap VirtuaL Text" })
+
+require("cmp").setup({
+  enabled = function()
+    return vim.api.nvim_get_option_value("buftype", { buf = 0 }) ~= "prompt"
+        or require("cmp_dap").is_dap_buffer()
+  end
+})
+require("cmp").setup.filetype({ "dap-view", "dap-view-term", "dap-repl" }, {
+  sources = {
+    { name = "dap" },
+  },
+})
+
+-- reset K while in debug session
+local api = vim.api
+local keymap_restore = {}
+dap.listeners.after['event_initialized']['me'] = function()
+  for _, buf in pairs(api.nvim_list_bufs()) do
+    local keymaps = api.nvim_buf_get_keymap(buf, 'n')
+    for _, keymap in pairs(keymaps) do
+      if keymap.lhs == "K" then
+        table.insert(keymap_restore, keymap)
+        api.nvim_buf_del_keymap(buf, 'n', 'K')
+      end
+    end
+  end
+  api.nvim_set_keymap(
+    'n', 'K', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
 end
 
---[[ vim.api.nvim_create_autocmd({'BufEnter'}, {
- pattern = {"*.hl", "hypr*.conf",},
- callback = function(event)
-   print(string.format("starting hyprls for %s", vim.inspect(event)))
-   vim.lsp.start {
-     name = "hyprlang",
-     cmd = {"hyprls"},
-     root_dir = vim.fn.getcwd(),
-   }
- end
-}) ]]
+dap.listeners.after['event_terminated']['me'] = function()
+  for _, keymap in pairs(keymap_restore) do
+    if keymap.rhs then
+      api.nvim_buf_set_keymap(
+        keymap.buffer,
+        keymap.mode,
+        keymap.lhs,
+        keymap.rhs,
+        { silent = keymap.silent == 1 }
+      )
+    elseif keymap.callback then
+      vim.keymap.set(
+      keymap.mode,
+      keymap.lhs,
+      keymap.callback,
+      { buffer = keymap.buffer, silent = keymap.silent == 1 }
+      )
+    end
+  end
+  keymap_restore = {}
+end
